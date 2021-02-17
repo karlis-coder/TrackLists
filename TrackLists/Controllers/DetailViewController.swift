922951708bb24c64fc726b0c6703392d7d6b69dd
Karlis B <karlis.butins@gmail.com>
2021-02-17 14:47:34 +0200

    tracklists commit

---
+++ TrackLists/Controllers/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  TrackLists
+//
+//  Created by Karlis Butins on 03/02/2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    
+    var song: Song!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if song != nil{
+            trackImageView.image = UIImage(named: song.cover)
+            trackNameLabel.text = song.track + " - " + song.album
+            trackNameLabel.numberOfLines = 0
+            
+        }
+     }
+    
+
+}
